a924868a44d4e534013bff149497abb61f7a508e
codewithaqsa <mmuneerkhan1963@gmail.com>
2024-12-02 10:13:10 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+
+  
+  return (
+    <div className=" flex justify-center items-center h-screen">
+     <div className="w-80 h-80 bg-red-600 text-center content-center sm:bg-green-500 lg:bg-blue-500 lg:text-2xl">
+      BOX-1
+     </div>
+    </div>
+  )
+}
